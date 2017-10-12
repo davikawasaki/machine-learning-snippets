7df9a9d6bd517bae365378ed2728f1faec17fe4a
Davi Kawasaki <davishinjik@gmail.com>
2017-10-11 21:24:35 -0300

    CSV percentage split

---
+++ README.md
@@ -0,0 +1,50 @@
+# Python Snippets for Machine Learning Learning
+
+This repository covers some developed/in development snippets used for machine learning in Python, such as: CSV to ARFF, CSV percentage split.
+
+# TECHNOLOGIES & LIBRARIES USED
+
+1) [Python 2.7](https://docs.python.org/2/)
+
+# SNIPPETS
+
+1.\ CSV percentage split (supervised learning): insert a CSV file with semicolon delimitators and split proportionally to each true label, exporting two files - one for training (normally 80%) and another for testing (normally 20%).
+
+# HOW TO USE
+
+1\. Set permissions to split-supervised-learning.py to run:
+
+```
+$ chmod +x split-supervised-learning.py
+```
+
+2\. Insert the CSV file to be splitted inside /data/raw.
+
+3\. Make sure the delimitators for each value is a semicolon and the header titles are between quotes.
+
+4\. Run python script with the true label name, the file name from /data/raw (without .csv extension), the training-rate and the testing rate. Examples:
+
+```
+$ ./split-supervised-learning.py lettr letters 80 20
+$ ./split-supervised-learning.py quality wine 90 10
+```
+
+# REFERENCES
+
+1) [Lichman, M. (2013). UCI Machine Learning Repository. Irvine, CA: University of California, School of Information and Computer Science.](http://archive.ics.uci.edu/ml)
+
+2) [Letter Recognition Data Set.](https://archive.ics.uci.edu/ml/datasets/Letter+Recognition)
+
+3) [P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis. Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.](https://archive.ics.uci.edu/ml/datasets/wine+quality)
+
+# COLABORATORS
+
+KAWASAKI, Davi // davishinjik [at] gmail.com
+
+FLAUSINO, Matheus // matheus.negocio [at] gmail.com
+
+# CONTACT & FEEDBACKS
+
+Feel free to contact or pull request me to any relevant updates you may enquire:
+
+KAWASAKI, Davi // davishinjik [at] gmail.com
---
+++ split-supervised-learning.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+
+import csv, sys
+
+class SplitBase:
+
+    def __init__( self, reader ):
+        self.reader = reader
+    
+    # Split and order each truelabel rows in grups
+    # Has truelabel group, truelabel index, truelabel object.
+    #
+    # @param self
+    # @param trueLabel
+    #
+    # @return trueLabelGroup
+    def split( self, trueLabel ):
+
+        tlgb = {}
+        for n, row in enumerate(self.reader):
+            # Save header row
+            if n == 0:
+                self.header = row
+                tli = self.getTrueLabelIndex(self.header, trueLabel)
+                if(tli == -1):
+                    raise Exception('There is not any truelabel as ' + trueLabel)
+                else:
+                    self.tli = tli
+            # Get other rows
+            else:
+                tlo = row[self.tli]
+                if tlo not in tlgb:
+                    tlgb[tlo] = list()
+                del(row[self.tli])
+                tlgb[tlo].append(row)
+
+        return tlgb
+    
+    # Split and order each truelabel rows in grups
+    # Has truelabel group, truelabel index, truelabel object.
+    #
+    # @param self
+    # @param trueLabelGroupedBy
+    # @param trainingRate
+    # @param dataFileName
+    #
+    # @return Boolean
+    def extractSaveBaseRates( self, tlgb, trRate, dfn ):
+        if not tlgb:
+            return False
+        if not trRate:
+            return False
+        else:
+            trGroup = []
+            ttGroup = []
+            
+            trRate = trRate/100.0
+
+            for key, arrays in tlgb.items():
+                n = len(arrays)
+                trQty = int(n*trRate)
+                for i, item in enumerate(arrays):
+                    if i < trQty-1:
+                        trGroup.append(item)
+                    else:
+                        ttGroup.append(item)
+
+            # Generate training sample file                 
+            trOutFile  = open('data/output/' + dfn + '_training' + '.csv', "wb")
+            writer = csv.writer(trOutFile, delimiter=';')
+
+            # Header as the first row to save
+            writer.writerow(self.header)
+            # Save other rows
+            for item in trGroup:
+                writer.writerow(item)
+
+            trOutFile.close()
+
+            # Generate testing sample file                 
+            trOutFile  = open('data/output/' + dfn + '_testing' + '.csv', "wb")
+            writer = csv.writer(trOutFile, delimiter=';')
+
+            # Header as the first row to save
+            writer.writerow(self.header)
+            # Save other rows
+            for item in ttGroup:
+                writer.writerow(item)
+
+            trOutFile.close()
+            
+            return True
+
+    # Return true label column index inside csv file
+    #
+    # @param self
+    # @param header
+    # @param trueLabel
+    #
+    # @return index (-1 for non-existent)
+    def getTrueLabelIndex ( self, header, trueLabel ):
+        colnum = 0
+        for col in header:
+            if col == trueLabel:
+                return colnum
+        return -1
+
+################
+# Main process #
+################
+
+# truelabel and datafile
+tl = sys.argv[1]
+df = sys.argv[2]
+
+# training and testing rates
+trRate = int(sys.argv[3])
+ttRate = int(sys.argv[4])
+
+if((trRate + ttRate) != 100):
+    print 'Training and Testing rates must complete 100%! Try it again.'
+elif(ttRate > trRate):
+    print 'Testing rate must not be bigger than Training rate! Try it again.'
+else:    
+    idf = open('data/raw/' + df + '.csv', 'rU')
+    reader = csv.reader(idf, delimiter=';')
+
+    splitBase = SplitBase(reader)
+    tlgb = splitBase.split(tl)
+    #print tlgb
+
+    if splitBase.extractSaveBaseRates(tlgb, trRate, df):
+        print 'Successful extraction of ' + df + ' file!'
+    else:
+        print 'There was an error in the extraction of ' + df + ' file!'
+    idf.close()
